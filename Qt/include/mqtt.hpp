83fd569a20efb3f2fb9466a700a0506ac919495a
Marcin Bober <mbober1@gmail.com>
2021-05-29 17:34:36 +0200

    cleanup

--- Qt/include/mqtt.hpp
+++ Qt/include/mqtt.hpp
@@ -10,8 +10,4 @@ public:
     explicit Mqtt(QObject *parent = nullptr);
     ~Mqtt();
     void connectToHost(const QString &hostname, const uint16_t port, const QString &username, const QString &password);
-
-public slots:
-
-private:
 };
