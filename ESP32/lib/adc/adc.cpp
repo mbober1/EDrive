6850dafbb0e5b8cb09be25dfc5211da386772c4b
GZ9FT3 <marcin.bober@merit-automotive.com>
2021-10-12 08:58:15 +0200

    adc refactor

--- ESP32/lib/adc/adc.cpp
+++ ESP32/lib/adc/adc.cpp
@@ -4,6 +4,10 @@
 #include "freertos/queue.h"
 #include "task_mngm.hpp"
 
+
+/**
+ * Contructs ADC object and set it up.
+ */
 myADC::myADC() 
 {
     adc1_config_width(ADC_WIDTH_BIT_12);
@@ -12,15 +16,30 @@ myADC::myADC()
     for (int i = 0; i < ADC_DATA_SIZE; i++) nextMeasurement();
 }
 
-void myADC::nextMeasurement() {
+/**
+ * Adds another measurement to the buffer
+ */
+void myADC::nextMeasurement() 
+{
     static int idx;
     this->data[idx++] = adc1_get_raw(ADC1_CHANNEL_0);
     if(idx >= ADC_DATA_SIZE) idx = 0;
 }
 
-float myADC::getVoltage() {
+/**
+ * Calculates the average of the measurements in the buffer 
+ * and convert the result into SI units.
+ * @return Voltage in V
+ */
+float myADC::getVoltage() 
+{
     uint32_t adc_reading = 0;
-    for (size_t i = 0; i < ADC_DATA_SIZE; i++) adc_reading += this->data[i];
+    
+    for (size_t i = 0; i < ADC_DATA_SIZE; i++) 
+    {
+        adc_reading += this->data[i];
+    }
+
     return ((float)adc_reading / ADC_DATA_SIZE) / CONVERSION_FACTOR;
 }
 
