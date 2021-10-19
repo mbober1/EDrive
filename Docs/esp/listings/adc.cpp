282e51a210523d1c46267182a626fcf85753a475
Marcin Bober <mbober1@gmail.com>
2021-10-19 19:08:39 +0200

    update doc

--- Docs/esp/listings/adc.cpp
+++ Docs/esp/listings/adc.cpp
@@ -1,22 +1,29 @@
 #include "adc.hpp"
+#include "driver/adc.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "task_mngm.hpp"
+
 
 /**
  * Contructs ADC object and set it up.
  */
-myADC::myADC() {
+myADC::myADC() 
+{
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_2_5);
+    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_6);
 
-    for (int i = 0; i < dataSize; i++) nextMeasurement();
+    for (int i = 0; i < ADC_DATA_SIZE; i++) nextMeasurement();
 }
 
 /**
  * Adds another measurement to the buffer
  */
-void myADC::nextMeasurement() {
+void myADC::nextMeasurement() 
+{
     static int idx;
     this->data[idx++] = adc1_get_raw(ADC1_CHANNEL_0);
-    if(idx >= dataSize) idx = 0;
+    if(idx >= ADC_DATA_SIZE) idx = 0;
 }
 
 /**
@@ -24,11 +31,37 @@ void myADC::nextMeasurement() {
  * and convert the result into SI units.
  * @return Voltage in V
  */
-float myADC::getVoltage() {
+float myADC::getVoltage() 
+{
     uint32_t adc_reading = 0;
-    for (size_t i = 0; i < dataSize; i++) 
+    
+    for (size_t i = 0; i < ADC_DATA_SIZE; i++) 
     {
         adc_reading += this->data[i];
     }
-    return ((float)((adc_reading/dataSize) + this->conversion_offset) / this->conversion_factor);
+
+    return ((float)adc_reading / ADC_DATA_SIZE) / CONVERSION_FACTOR;
+}
+
+
+void batteryTask(void*)
+{
+    myADC battery;
+    int idx = 0;
+    float voltage;
+
+    while (1)
+    {
+        battery.nextMeasurement();
+
+        if (idx++ > 10)
+        {
+            idx = 0;
+            voltage = battery.getVoltage();
+            xQueueSendToBack(voltageQueue, &voltage, 0);
+        }
+        
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+    vTaskDelete(NULL);
 }
--- Docs/esp/listings/pid.cpp
+++ Docs/esp/listings/pid.cpp
@@ -1,45 +1,124 @@
+#include "pid.hpp"
+#include "config.hpp"
+#include "task_mngm.hpp"
+#include "mqtt.hpp"
+
 
 /**
- * Compute PID.
- * @param setpoint Setpoint
+ * @brief PID class constructor.
+ * @param encoder Encoder object to use.
  */
-void IRAM_ATTR motor::compute(const int &setpoint) {
+Pid::Pid(const Encoder &encoder, const Pid_config_t *config) : pcnt_unit(encoder.pcnt_unit) 
+{
+    this->set_parameters(config);
+}
+
+
+/**
+ * @brief Compute PID result.
+ * @param setpoint Setpoint.
+ */
+int16_t Pid::compute(const int &setpoint) {
     int16_t input;
-    pcnt_get_counter_value(this->encoder, &input);
-    pcnt_counter_clear(this->encoder);
+    GET_ENCODER_VALUE(PCNT_UNIT_0, &input);
 
     int epsilon = setpoint - input;
 
-    this->integralError+= epsilon;
-    if(this->integralError > MAX_INTEGRAL) this->integralError = MAX_INTEGRAL;
-    else if(this->integralError < -MAX_INTEGRAL) this->integralError = -MAX_INTEGRAL;
-    this->derivativeError = epsilon - epsilonOld;
-    this->epsilonOld = epsilon;
-    
+    // calculate integral error
+    this->integral_error+= epsilon;
+    NORMALIZE_I(this->integral_error);
 
-    int16_t p = this->kp * epsilon;
-    int16_t i = this->ki * this->integralError;
-    int16_t d = this->kd * this->derivativeError;
+    // calculate derivative error
+    this->derivative_error = epsilon - last_epsilon;
 
+    // save last epsilon
+    this->last_epsilon = epsilon;
+
+    // calculate PID
+    int16_t p = this->config.kp * epsilon;
+    int16_t i = this->config.ki * this->integral_error;
+    int16_t d = this->config.kd * this->derivative_error;
     int16_t pid = p + i + d;
-    if(pid > MAX_PID) pid = MAX_PID;
-    else if(pid < MIN_PID) pid = MIN_PID;
+    NORMALIZE_PID(pid);
 
-    if(pid > 0) {
-        mcpwm_set_signal_low(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_GEN_B);
-        mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_GEN_A, pid);
-        mcpwm_set_duty_type(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_GEN_A, MCPWM_DUTY_MODE_0);
+    return pid;
+}
 
-    } else if(pid < 0) {
-        mcpwm_set_signal_low(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_GEN_A);
-        mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_GEN_B, -pid);
-        mcpwm_set_duty_type(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_GEN_B, MCPWM_DUTY_MODE_0); 
 
-    } else {
-        mcpwm_set_signal_low(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_GEN_A);
-        mcpwm_set_signal_low(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_GEN_B);
+/**
+ * @brief Setup PID parameters
+ * @param config Pointer to config struct.
+ */
+void Pid::set_parameters(const Pid_config_t *config) 
+{
+    memcpy(&(this->config), config, sizeof(Pid_config_t));
+}
+
+
+/**
+ * @brief Motor control task.
+ */
+void motorTask(void*)
+{
+    Pid_message_t mess;
+    Pid_config_t config =        // create PID config object
+    {
+        .kp = DEFAULT_PID_KP, 
+        .ki = DEFAULT_PID_KI, 
+        .kd = DEFAULT_PID_KD, 
+    };
+    
+    // init engine object
+    Motor engine(MOTOR_UNIT, MOTOR_IN1, MOTOR_IN2, MOTOR_TIMER); 
+    
+    // init encoder object
+    Encoder encoder(ENC_A, ENC_B, ENC_PCNT_UNIT);  
+    
+    // init PID object
+    Pid pid(encoder, &config);                                   
+
+    TickType_t xLastWakeTime;
+    int16_t pid_result;
+
+    int setpoint = 0;
+
+    while (true)
+    {
+        xLastWakeTime = xTaskGetTickCount();
+
+        if (pdTRUE == xQueueReceive(pidQueue, &mess, 0L)) 
+        {
+            switch (mess.parameter)
+            {
+            case SETPOINT_PARAMETER:
+                setpoint = RPMtoTick(mess.value);
+                break;
+            
+            case KP_PARAMETER:
+                config.kp = mess.value;
+                break;
+            
+            case KI_PARAMETER:
+                config.ki = mess.value;
+                break;
+            
+            case KD_PARAMETER:
+                config.kd = mess.value;
+                break;
+            
+            default:
+                break;
+            }
+
+            pid.set_parameters(&config);
+        }
+
+        pid_result = pid.compute(setpoint);
+        engine.set_duty(pid_result);
+        
+        // set PID loop to 10ms
+        vTaskDelayUntil(&xLastWakeTime, PID_LOOP_PERIOD);
     }
 
-    xQueueSendToBack(pulsesQueue, &input, 0);
-    xQueueSendToBack(powerQueue, &pid, 0);
+   vTaskDelete(NULL);
 }
